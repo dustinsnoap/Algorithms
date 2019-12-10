adc37dded17085e4c120acd4d28329c9df90b13b
unknown <afrobacon@gmail.com>
2019-12-10 17:29:34 -0500

    eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,16 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  ns = [1,1,2]
+  combos = 0
+  if n < 3: combos = ns[n]
+  else:
+    for _ in range(3, n):
+      ns.insert(3,ns[0] + ns[1] + ns[2])
+      ns.pop(0)
+    for n in ns:
+      combos += n
+  return combos
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
